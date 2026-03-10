429a130677124fd394ee9574f7bcc0e6d6324e3d
Octerry <taaniyu@gmail.com>
2026-03-10 15:35:07 +0100

    Ajout de l'exercice variable css

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ const nameInput = document.getElementById("name_input")
 const welcomeTxt = document.getElementById("welcome_txt")
 const nameValue = localStorage.getItem('name')
 
+const root = document.documentElement
+const varInputs = document.getElementsByClassName('var_input')
+const textInput = varInputs[0].children[1]
+const borderInput = varInputs[1].children[1]
+const paddingInput = varInputs[2].children[1]
+const colorInput = varInputs[3].children[1]
+
 button.addEventListener('click', function(){
     popupWindow.classList.add('view')
 
@@ -27,3 +34,36 @@ saveButton.addEventListener('click', function(){
     }
 })
 
+
+let size = textInput.value/5 + 10 + "px"
+let radius = borderInput.value/5 + "px"
+let padding = paddingInput.value/10 + 5 + "px"
+let color = colorInput.value
+root.style.setProperty("--buttonTextSize", size)
+root.style.setProperty("--buttonBorderRadius", radius)
+root.style.setProperty("--buttonPadding", padding)
+root.style.setProperty("--buttonBgColor", color)
+
+textInput.addEventListener('input', function(){
+    let size = this.value/5 + 10 + "px"
+    root.style.setProperty("--buttonTextSize", size)
+    varInputs[0].children[2].innerHTML = size
+})
+
+borderInput.addEventListener('input', function(){
+    let radius = this.value/5 + "px"
+    root.style.setProperty("--buttonBorderRadius", radius)
+    varInputs[1].children[2].innerHTML = radius
+})
+
+paddingInput.addEventListener('input', function(){
+    let padding = this.value/10 + 5 + "px"
+    root.style.setProperty("--buttonPadding", padding)
+    varInputs[2].children[2].innerHTML = padding
+})
+
+colorInput.addEventListener('input', function(){
+    let color = this.value
+    console.log(color)
+    root.style.setProperty("--buttonBgColor", color)
+})
