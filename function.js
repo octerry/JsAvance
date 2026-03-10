2eb7a8b37df387f57984617a66357ef2cb43e435
Octerry <taaniyu@gmail.com>
2026-03-10 12:30:50 +0100

    Small changes

--- function.js
+++ function.js
@@ -3,10 +3,8 @@ const popupWindow = document.getElementsByClassName('popup')[0]
 
 button.addEventListener('click', function(){
     popupWindow.classList.add('view')
-    console.log('fait')
 
     setTimeout(() => {
         popupWindow.classList.remove('view')
-        console.log('good')
     }, 3000);
 })
